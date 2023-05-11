9b13177ac3584e5475b1bb2360838758de164594
Allen Johnson <webequate@gmail.com>
2023-05-10 21:46:48 -0600

    Trying to debug

--- pages/history/[id].tsx
+++ pages/history/[id].tsx
@@ -117,7 +117,7 @@ export const getStaticProps: GetStaticProps<ProjectProps> = async ({
       props: {
         name: JSON.parse(JSON.stringify(basics.name)),
         socialLinks: JSON.parse(JSON.stringify(basics.socialLinks)),
-        projects: projects,
+        projects: JSON.parse(JSON.stringify(projects)),
         project: JSON.parse(JSON.stringify(project)),
       },
       revalidate: 60,
