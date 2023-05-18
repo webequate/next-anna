93165935f89aeb2673194d24ab37d603a47eb694
Allen Johnson <webequate@gmail.com>
2023-05-18 01:47:01 -0600

    Adjust SVG title image

--- components/AnnaEliseJohnson.tsx
+++ components/AnnaEliseJohnson.tsx
@@ -2,10 +2,10 @@
 const AnnaEliseJohnson = () => {
   return (
     <svg
-      viewBox="0 0 186 8"
+      viewBox="0 0 186 14"
       className="w-full lg:w-4/5 h-auto mx-auto pt-4 pb-2 sm:pb-2"
     >
-      <text x="0" y="8" fill="currentColor" className="montserrat">
+      <text x="0" y="12" fill="currentColor" className="montserrat">
         ANNA ELISE JOHNSON
       </text>
     </svg>
